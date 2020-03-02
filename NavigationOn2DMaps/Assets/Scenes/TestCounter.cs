5df141ee28eb603a2676638be5e70333705516ab
Remigiusz Ganiec <remekgc@gmail.com>
2020-03-02 19:30:26 +0100

    update

---
+++ NavigationOn2DMaps/Assets/Scenes/TestCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestCounter : MonoBehaviour
+{
+    public TextMeshProUGUI counterText;
+    public int counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void counterListener()
+    {
+        counter++;
+        counterText.text = counter.ToString();
+    }
+}
