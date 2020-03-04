609e8e38ff93f40e75c27edbe1913f838a564bd4
Remigiusz Ganiec <remekgc@gmail.com>
2020-03-04 23:44:20 +0100

    Created Android studio project
    Created an Andorid studio project in order to write a library required to get the wifi SSID's and signal strength

---
+++ WifiManagerPlugin/wifimanagerlibrary/src/main/java/com/example/wifimanagerlibrary/AndroidWifiManager.java
@@ -0,0 +1,15 @@
+package com.example.wifimanagerlibrary;
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+public class AndroidWifiManager {
+    public void LogWifiManager(){
+        Log.d("Unity", "Hello from wifi manager");
+    }
+}
