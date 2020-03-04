6d70310d9f4df05b26b351e2d7fdcdc3ccaa45d4
Remigiusz Ganiec <remekgc@gmail.com>
2020-03-05 00:46:48 +0100

    simple java plugin example

---
+++ NavigationOn2DMap/Assets/Scripts/Wifi/testWifiPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testWifiPlugin : MonoBehaviour
+{
+
+    private AndroidJavaObject javaClass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        javaClass = new AndroidJavaObject("com.example.wifimanagerlibrary.AndroidWifiManager");
+        javaClass.Call("LogWifiManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
