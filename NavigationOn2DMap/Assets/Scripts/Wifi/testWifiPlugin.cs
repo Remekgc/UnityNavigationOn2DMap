61f5dfe1894c40939bf38c664f20d00f136050a5
Remigiusz Ganiec <remekgc@gmail.com>
2020-03-05 14:09:20 +0100

    Update to Android plugin
    Some more examples of Java plugin usage from Unity.

--- NavigationOn2DMap/Assets/Scripts/Wifi/testWifiPlugin.cs
+++ NavigationOn2DMap/Assets/Scripts/Wifi/testWifiPlugin.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class testWifiPlugin : MonoBehaviour
 {
+    public TextMeshProUGUI wifiManagerText;
 
     private AndroidJavaObject javaClass;
 
-    // Start is called before the first frame update
     void Start()
     {
         javaClass = new AndroidJavaObject("com.example.wifimanagerlibrary.AndroidWifiManager");
         javaClass.Call("LogWifiManager");
+        javaClass.Call("LogSentFromUnity", "Remi working here");
+        wifiManagerText.text = javaClass.Call<string>("SendDataToUnity", "Hello Android, I'm Unity\n");
+        // Calling java function that calls Unity function - for example, without any reason.
+        javaClass.Call("callUnityFunctionWithoutParameter");
+        javaClass.Call("callUnityFunctionWithParameter", "Hello there, I'm General Kenobi\n");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void testUnityFunctionFromAndroid()
     {
-        
+        wifiManagerText.color = Color.black;
+    }
+
+    public void testUnityFunctionFromAndroid2(string msg)
+    {
+        wifiManagerText.text += msg;
     }
 }
--- WifiManagerPlugin/wifimanagerlibrary/src/main/java/com/example/wifimanagerlibrary/AndroidWifiManager.java
+++ WifiManagerPlugin/wifimanagerlibrary/src/main/java/com/example/wifimanagerlibrary/AndroidWifiManager.java
@@ -1,15 +1,31 @@
 package com.example.wifimanagerlibrary;
-import android.Manifest;
-import android.content.Context;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.net.wifi.WifiManager;
-import android.os.Build;
-import android.os.Bundle;
+
 import android.util.Log;
 
+import com.unity3d.player.UnityPlayer;
+
 public class AndroidWifiManager {
+
     public void LogWifiManager(){
-        Log.d("Unity", "Hello from wifi manager");
+        Log.d("UnityWifiManager", "Hello from wifi manager");
+        Log.d("UnityWifiManager", "Welp");
+    }
+
+    public void LogSentFromUnity(String unityLog){
+        Log.d("UnityWifiManager", unityLog);
+    }
+
+    public String SendDataToUnity(String textFromUnity){
+        return textFromUnity + "Hello Unity, I'm Android\n";
+    }
+
+    public void callUnityFunctionWithoutParameter(){
+        // make call to Unity function
+        UnityPlayer.UnitySendMessage("WifiManagerPlugin", "testUnityFunctionFromAndroid", "");
+    }
+
+    public void callUnityFunctionWithParameter(String unityLog){
+        // make call to Unity function
+        UnityPlayer.UnitySendMessage("WifiManagerPlugin", "testUnityFunctionFromAndroid2", unityLog);
     }
 }
