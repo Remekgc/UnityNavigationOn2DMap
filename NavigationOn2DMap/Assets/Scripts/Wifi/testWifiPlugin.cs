549eff3af10497df6940d0bce639d001bcaeb27c
Remigiusz Ganiec <remekgc@gmail.com>
2020-03-06 10:05:35 +0100

    wifi list scanner ready

--- NavigationOn2DMap/Assets/Scripts/Wifi/testWifiPlugin.cs
+++ NavigationOn2DMap/Assets/Scripts/Wifi/testWifiPlugin.cs
@@ -12,12 +12,18 @@ public class testWifiPlugin : MonoBehaviour
     void Start()
     {
         javaClass = new AndroidJavaObject("com.example.wifimanagerlibrary.AndroidWifiManager");
-        javaClass.Call("LogWifiManager");
-        javaClass.Call("LogSentFromUnity", "Remi working here");
-        wifiManagerText.text = javaClass.Call<string>("SendDataToUnity", "Hello Android, I'm Unity\n");
+        javaClass.Call("setWifiScanReceiver");
+        InvokeRepeating("getWifiList", 1, 10);
+        //javaClass.Call("LogSentFromUnity", "Test Unity -> Android communication");
+        //wifiManagerText.text = javaClass.Call<string>("SendDataToUnity", "Hello Android, I'm Unity\n");
         // Calling java function that calls Unity function - for example, without any reason.
-        javaClass.Call("callUnityFunctionWithoutParameter");
-        javaClass.Call("callUnityFunctionWithParameter", "Hello there, I'm General Kenobi\n");
+        //javaClass.Call("callUnityFunctionWithoutParameter");
+        //javaClass.Call("callUnityFunctionWithParameter", "Hello there, I'm General Kenobi\n");
+    }
+
+    public void getWifiList()
+    {
+        wifiManagerText.text = javaClass.Call<string>("getWifiScan");
     }
 
     public void testUnityFunctionFromAndroid()
--- WifiManagerPlugin/wifimanagerlibrary/src/main/java/com/example/wifimanagerlibrary/AndroidWifiManager.java
+++ WifiManagerPlugin/wifimanagerlibrary/src/main/java/com/example/wifimanagerlibrary/AndroidWifiManager.java
@@ -1,18 +1,62 @@
 package com.example.wifimanagerlibrary;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.util.Log;
 
 import com.unity3d.player.UnityPlayer;
 
-public class AndroidWifiManager {
+import java.util.List;
 
-    public void LogWifiManager(){
-        Log.d("UnityWifiManager", "Hello from wifi manager");
-        Log.d("UnityWifiManager", "Welp");
+public class AndroidWifiManager {
+    private Context context = UnityPlayer.currentActivity.getApplicationContext();
+    private WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    private String wifiScan = "", TAG = "UnityWifiManager";
+    private IntentFilter intentFilter = new IntentFilter();
+
+    private BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context c, Intent intent) {
+            boolean success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
+            if (success) {
+                List<ScanResult> results = wifiManager.getScanResults();
+                Log.d(TAG, "Scan Successful, new results:");
+                //Log.d(TAG, results.toString());
+                wifiScan = "";
+                for (ScanResult result:results) {
+                    wifiScan += "{SSID : " + result.SSID + "} - {RSSI : " + result.level + "}\n";
+                }
+            } else {
+                // scan failure handling
+                List<ScanResult> results = wifiManager.getScanResults();
+                Log.d(TAG, "Scan failed, old results:");
+                //Log.d(TAG, results.toString());
+                wifiScan = "";
+                for (ScanResult result:results) {
+                    wifiScan += "{SSID : " + result.SSID + "} - {RSSI : " + result.level + "}\n";
+                }
+            }
+        }
+    };
+
+    public void setWifiScanReceiver(){
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        context.registerReceiver(wifiScanReceiver, intentFilter);
+        Log.d(TAG, "Wifi manager started");
+    }
+
+    public String getWifiScan(){
+        Log.d(TAG, "Trying to Scan");
+        wifiManager.startScan();
+        return wifiScan;
     }
 
     public void LogSentFromUnity(String unityLog){
-        Log.d("UnityWifiManager", unityLog);
+        Log.d(TAG, unityLog);
     }
 
     public String SendDataToUnity(String textFromUnity){
