3aeaa51237b5df57d0a87a86f09ebdcc893b4d88
Hubert Lichaczewski <hubert.lichaczewski@gmail.com>
2020-04-01 16:21:33 +0200

    Beacon on map

---
+++ NavigationOn2DMap/Assets/Scenes/Hubert/BeaconControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeaconControl : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject beaconIcon;
+    public float x;
+    public float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject beacon = Instantiate(beaconIcon);
+        beacon.transform.position = new Vector3(x, y, 2); //cos zjebane z tym 2d albo warstwami
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
